f5def37316daa58c575938a7d39ee876acd6e9fa
Ava <PercentMage09@gmail.com>
2023-09-23 20:19:11 +0100

    Updated game
    Created CameraShake.cs
    Created white material
    Created red material
    Imported legacy effect pack
    Added smoke to scene

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public Transform pivotPoint;
+    public float wobbleAmount = 0.1f;
+    public float wobbleSpeed = 2f;
+    public float rotationAmount = 2f;
+
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        float wobbleX = Mathf.Sin(Time.time * wobbleSpeed) * wobbleAmount;
+        float wobbleY = Mathf.Sin(Time.time * wobbleSpeed * 2) * (wobbleAmount / 2);
+        float wobbleZ = 0f;
+
+        Quaternion rotation = Quaternion.Euler(
+            Random.Range(-rotationAmount, rotationAmount),
+            Random.Range(-rotationAmount, rotationAmount),
+            Random.Range(-rotationAmount, rotationAmount)
+        );
+
+        Vector3 targetPosition = pivotPoint.position + new Vector3(wobbleX, wobbleY, wobbleZ);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+    }
+}

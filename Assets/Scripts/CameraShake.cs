e75b3cbd12d4d809f020592e0e2c6e38513f38ad
Ava <PercentMage09@gmail.com>
2023-09-24 17:59:09 +0100

    Updated timer
    Updated placeholder text for time entry box
    Changed colour scheme of sidebar and items within to better fit app
    Imported music
    Updated camera shake script to have a toggle

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -1,3 +1,4 @@
+using Palmmedia.ReportGenerator.Core.Reporting.Builders;
 using UnityEngine;
 
 public class CameraShake : MonoBehaviour
@@ -10,6 +11,8 @@ public class CameraShake : MonoBehaviour
     private Vector3 initialPosition;
     private Quaternion initialRotation;
 
+    public bool shakeEnabled = true;
+
     private void Start()
     {
         initialPosition = transform.position;
@@ -18,19 +21,32 @@ public class CameraShake : MonoBehaviour
 
     private void Update()
     {
-        float wobbleX = Mathf.Sin(Time.time * wobbleSpeed) * wobbleAmount;
-        float wobbleY = Mathf.Sin(Time.time * wobbleSpeed * 2) * (wobbleAmount / 2);
-        float wobbleZ = 0f;
+        if(shakeEnabled == true)
+        {
+            float wobbleX = Mathf.Sin(Time.time * wobbleSpeed) * wobbleAmount;
+            float wobbleY = Mathf.Sin(Time.time * wobbleSpeed * 2) * (wobbleAmount / 2);
+            float wobbleZ = 0f;
 
-        Quaternion rotation = Quaternion.Euler(
-            Random.Range(-rotationAmount, rotationAmount),
-            Random.Range(-rotationAmount, rotationAmount),
-            Random.Range(-rotationAmount, rotationAmount)
-        );
+            Quaternion rotation = Quaternion.Euler(
+                Random.Range(-rotationAmount, rotationAmount),
+                Random.Range(-rotationAmount, rotationAmount),
+                Random.Range(-rotationAmount, rotationAmount)
+            );
 
-        Vector3 targetPosition = pivotPoint.position + new Vector3(wobbleX, wobbleY, wobbleZ);
+            Vector3 targetPosition = pivotPoint.position + new Vector3(wobbleX, wobbleY, wobbleZ);
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+        }
+        else
+        {
+            transform.position = pivotPoint.position;
+        }
+        
+    }
+
+    public void ToggleShake()
+    {
+        shakeEnabled = !shakeEnabled;
     }
 }
